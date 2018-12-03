fbdf4f542f5e66d368b0d7919d1a755edc642cdc
Nehsus <sushenksharma@gmail.com>
2018-12-03 17:09:02 +0530

    v1.0

---
+++ application.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+
+application = Flask(__name__)  # Change assignment here
+
+
+@application.route("/")        # Change your route statements
+def hello():
+    return render_template('index.html')
+
+@application.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@application.route('/team')
+def team():
+    return render_template('team.html')
+
+if __name__ == "__main__":
+    application.run()
